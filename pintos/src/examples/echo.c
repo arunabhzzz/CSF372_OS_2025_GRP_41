2df4fa4ae08d4e35eb6252e58dd3325d3110c9e2
arunabhzzz <sarunabh64@gmail.com>
2025-09-01 14:02:25 +0530

    trying enhanced \r \t not working yet needs fixing

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
-#include <syscall.h>
+#include<syscall.h>
+#include <stdlib.h>
 
-int
-main (int argc, char **argv)
-{
-  int i;
+int main(int argc, char **argv) {
+    // Loop through each argument (skip argv[0], the program name)
+    for (int i = 1; i < argc; i++) {
+        const char *arg = argv[i];
 
-  for (i = 0; i < argc; i++)
-    printf ("%s ", argv[i]);
-  printf ("\n");
+        for (int j = 0; arg[j] != '\0'; j++) {
+            if (arg[j] == '\\') {
+                // Handle escape sequences
+                j++;
+                switch (arg[j]) {
+                    case 'n': putchar('\n'); break;
+                    case 't': putchar('\t'); break;
+                    case 'r': putchar('\r'); break;
+                    case '\\': putchar('\\'); break;
+                    case '"': putchar('"'); break;
+                    case '\0': putchar('\\'); j--; break; // trailing backslash
+                    default: putchar('\\'); putchar(arg[j]); break; // unknown escape
+                }
+            } else {
+                putchar(arg[j]);
+            }
+        }
 
-  return EXIT_SUCCESS;
+        if (i < argc - 1) putchar(' '); // space between arguments
+    }
+
+    putchar('\n'); // final newline
+    return 0; // EXIT_SUCCESS may not be defined in Pintos
 }
